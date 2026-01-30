9386df7cb37edb13a03e54d340a65fe7ce0930b5
FlitneyR <robert.ogflitney@gmail.com>
2026-01-30 19:18:40 +0000

    Add basic player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+
+    InputAction moveAction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveValue = moveAction.ReadValue<Vector2>() * moveSpeed;
+
+        transform.Translate(moveValue);
+    }
+}
